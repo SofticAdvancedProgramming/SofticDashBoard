813fad0e7d2166a7cd85f25ef4f24d8c47ace139
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-07-29 14:34:50 +0300

    make department

---
+++ src/app/core/dashboard/company/components/department/department-overview/department-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './department-overview.component.html',
+  styleUrl: './department-overview.component.css'
+})
+export class DepartmentOverviewComponent {
+
+}
