d38dcf94be1e1c2c057dfc92c98f52b5c3d55a14
Nabil warda <165319547+NabilWarda24@users.noreply.github.com>
2024-07-30 22:54:37 +0300

    0

--- src/app/core/dashboard/company/components/position/add-position/add-position.component.ts
+++ src/app/core/dashboard/company/components/position/add-position/add-position.component.ts
@@ -1,12 +1,17 @@
-import { Component, Output, EventEmitter, Input, inject } from '@angular/core';
+import { Component, Output, EventEmitter, Input, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PositionTypeService } from '../../../../../../services/lockupsServices/positionTypeService/position-type.service';
+import { DepartmentService } from '../../../../../../services/lockupsServices/DepartmentService/department.service';
+import { Department } from '../../../../../../../models/department';
+
+
 interface Position{
   id:number,
   name:string,
   nameAr:string
 }
+
 @Component({
   selector: 'app-add-position',
   standalone: true,
@@ -14,20 +19,34 @@ interface Position{
   templateUrl: './add-position.component.html',
   styleUrls: ['./add-position.component.css']
 })
-export class AddPositionComponent {
+export class AddPositionComponent implements OnInit {
   @Output() action = new EventEmitter<boolean>();
-  @Input() companyId?:string=''
-  types:Position[]=[]
-  positionTypeService=inject(PositionTypeService);
+  @Input() companyId?: string = '';
+  types: Position[] = [];
+  departments: Department[] = [];
+  selectedPositionId: string = '';
+  selectedDepId: string = '';
+  positionTypeService = inject(PositionTypeService);
+  departmentsService = inject(DepartmentService);
+
   ngOnInit(): void {
     this.loadPositionTypes();
   }
 
   loadPositionTypes(): void {
-    this.positionTypeService.getPositionTypes({companyId:Number(this.companyId)}).subscribe({
+    this.positionTypeService.getPositionTypes({companyId:this.companyId}).subscribe({
       next: (response) => {
-        console.log("response:",response)
         this.types = response.data.list;
+      },
+      error: (err) => {
+        console.error('Error loading position types', err);
+      }
+    });
+  }
+  loadDepartments(): void {
+    this.departmentsService.getDepartment({companyId:this.companyId}).subscribe({
+      next: (response) => {
+        this.departments = response.data.list;
       },
       error: (err) => {
         console.error('Error loading position types', err);
@@ -41,5 +60,9 @@ export class AddPositionComponent {
 
   onBack(): void {
     this.action.emit(false);
+  }
+
+  trackById(index: number, item: Position): number {
+    return item.id;
   }
 }
