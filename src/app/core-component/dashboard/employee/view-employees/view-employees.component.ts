011a871d2319543ef7cbbcbd0f2f785eba08bbbb
Nabil warda <165319547+NabilWarda24@users.noreply.github.com>
2024-08-25 22:07:18 +0300

    0

--- src/app/core-component/dashboard/employee/view-employees/view-employees.component.ts
+++ src/app/core-component/dashboard/employee/view-employees/view-employees.component.ts
@@ -1,18 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ModernTableComponent } from "../../components/modern-table/modern-table.component";
 import { RouterLink } from '@angular/router';
-import { FormGroup } from '@angular/forms';
-import { branch } from '../../../../../models/branch';
+import { EmployeeService } from '../../../../services/employeeService/employee.service';
+import { employee } from '../../../../../models/employee';
 
 @Component({
-    selector: 'app-view-employees',
-    standalone: true,
-    templateUrl: './view-employees.component.html',
-    styleUrl: './view-employees.component.css',
-    imports: [ModernTableComponent ,RouterLink]
+  selector: 'app-view-employees',
+  standalone: true,
+  templateUrl: './view-employees.component.html',
+  styleUrls: ['./view-employees.component.css'],
+  imports: [ModernTableComponent, RouterLink]
 })
-export class ViewEmployeesComponent {
-    @Output() action = new EventEmitter<boolean>();
-      branches: branch[] = [];
-    @Input() companyId?: number ;
+export class ViewEmployeesComponent implements OnInit {
+  employeeService = Inject(EmployeeService);
+  companyId:Number=0;
+  employees:employee[]=[];
+  ngOnInit() {
+    this.loadEmployeesByCompany();
+  }
+
+  loadEmployeesByCompany() {
+    this.companyId = Number(localStorage.getItem('companyId'));
+    if (this.companyId) {
+      this.employeeService.loadEmployees({comapnyId:this.companyId}).subscribe(
+        (response: any) => {
+          this.employees = response;
+          console.log('Employees loaded:', this.employees);
+        },
+        (error: any) => {
+          console.error('Error loading employees', error);
+        }
+      );
+    } else {
+      console.warn('No company found in local storage');
+    }
+  }
 }
