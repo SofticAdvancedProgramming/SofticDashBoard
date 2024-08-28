a018086aca9c371e44a0472eb221632261f17034
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-08-28 17:00:41 +0300

    initially load personal infromation

--- src/app/core-component/dashboard/employee/employeeDetailsComponents/personal-information/personal-information.component.ts
+++ src/app/core-component/dashboard/employee/employeeDetailsComponents/personal-information/personal-information.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserDataService } from '../../../../../services/userDataService/user-data.service';
+import { PersonalInformation } from '../../../../../../models/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-personal-information',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './personal-information.component.html',
-  styleUrl: './personal-information.component.css'
+  styleUrls: ['./personal-information.component.css']
 })
-export class PersonalInformationComponent {
+export class PersonalInformationComponent implements OnInit {
+  personalInfo: PersonalInformation | null = null;
+  employeeId: number | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userDataService: UserDataService
+  ) {}
+
+  ngOnInit(): void {
+    this.employeeId = Number(this.route.snapshot.paramMap.get('id'));
+    if (this.employeeId) {
+      this.loadPersonalInformation();
+    } else {
+      console.warn('No employee ID found in the route parameters');
+    }
+  }
+
+  loadPersonalInformation(): void {
+    const requestPayload = {
+      id: this.employeeId
+    };
+
+    this.userDataService.loadPersonalInformation(requestPayload).subscribe(
+      (response) => {
+        if (response && response.list && response.list.length > 0) {
+          this.personalInfo = response.list[0] as PersonalInformation;
+          // Check if birthDate is a placeholder or invalid and handle accordingly
+          if (this.personalInfo.birthDate === '0001-01-01T00:00:00') {
+           }
+          console.log(response);
+        }
+      },
+      (error) => {
+        console.error('Error loading personal information:', error);
+      }
+    );
+  }
 }
---
+++ src/app/services/userDataService/user-data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ApiCall } from '../../core/services/http-service/HttpService';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+  private PersonalInformationUrl = `${environment.apiBaseUrl}Users`;
+
+  constructor(private apiCall: ApiCall) { }
+  loadPersonalInformation(request: any): Observable<any> {
+    return this.apiCall.request('POST', this.PersonalInformationUrl + '/GetPersonalInformation', request,);
+  }
+}
