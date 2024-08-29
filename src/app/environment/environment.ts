1e7b8b1ed4d047814751aaafec492293b17506bd
Nabil warda <165319547+NabilWarda24@users.noreply.github.com>
2024-08-30 00:56:35 +0300

    Solve link

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'https://attendancebackend.smartnetworkai.com/',
+  apiBaseUrl: 'https://attendancebackend.smartnetworkai.com/api/',
   dashboardBaseUrl: 'https://attendancebackend.smartnetworkai.com/',
   ElmedaniHub: 'https://sap-pro.xyz:44333/ElmedaniHub',
 };
