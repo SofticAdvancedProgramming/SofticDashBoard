cda88828465abbca9476533baffc2d3d30e58560
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-09-04 14:44:32 +0300

    custmize the show and hide in lockups

--- src/app/core-component/dashboard/setting/general-lookups/general-lookups.component.ts
+++ src/app/core-component/dashboard/setting/general-lookups/general-lookups.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NextButtonComponent } from '../components/next-button/next-button.component';
 import { LockupCardComponent } from "../../../../common-component/lockup-card/lockup-card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-general-lookups',
     standalone: true,
     templateUrl: './general-lookups.component.html',
     styleUrl: './general-lookups.component.css',
-    imports: [RouterLink, NextButtonComponent, LockupCardComponent]
+    imports: [RouterLink, NextButtonComponent, LockupCardComponent , CommonModule]
 })
 export class GeneralLookupsComponent {
-
+    role: any = JSON.parse(localStorage.getItem('roles')!);
 }
