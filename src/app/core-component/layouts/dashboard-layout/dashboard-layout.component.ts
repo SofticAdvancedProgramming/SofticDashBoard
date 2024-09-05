e3491433cb52ee6c87a3b9dabd5fd93d04d90186
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-09-05 17:24:16 +0300

    enhance the layout ui in small screens

--- src/app/core-component/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/core-component/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -3,14 +3,25 @@ import { FooterComponent } from '../../layout/footer/footer.component';
 import { NavbarComponent } from '../../layout/navbar/navbar.component';
 import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [SidebarComponent,NavbarComponent,FooterComponent,RouterOutlet],
+  imports: [SidebarComponent,NavbarComponent,FooterComponent,RouterOutlet, FormsModule , CommonModule],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
 export class DashboardLayoutComponent {
+  isArabic: boolean = false;
 
+  constructor(private translate: TranslateService) {
+    this.isArabic = this.translate.currentLang === 'ar';
+
+    this.translate.onLangChange.subscribe((event) => {
+      this.isArabic = event.lang === 'ar';
+    });
+  }
 }
