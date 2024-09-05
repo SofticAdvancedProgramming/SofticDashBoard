88daedb983fb15e7e33f2dd108d07b1b8b9aec44
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-09-05 17:08:47 +0300

    translate the sidebar

--- src/app/core-component/layout/sidebar/sidebar.component.ts
+++ src/app/core-component/layout/sidebar/sidebar.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthenticationService } from '../../../services/authenticationService/authentication.service';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
   role: any = JSON.parse(localStorage.getItem('roles')!);
-  companyId: string = localStorage.getItem('companyId')!
+  companyId: string = localStorage.getItem('companyId')!;
   isExpanded = false;
   isVisible = true;
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService, private translate: TranslateService) { }
 
   toggleSidebar() {
     this.isExpanded = !this.isExpanded;
