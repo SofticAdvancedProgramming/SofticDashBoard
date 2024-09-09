1873d9c00df2bbd943ab6e3448b51046294e1937
Nabil warda <165319547+NabilWarda24@users.noreply.github.com>
2024-09-09 23:41:24 +0300

    Home localization and return deleted part in add company

--- src/app/core-component/dashboard/components/cards/active-companies/active-companies.component.ts
+++ src/app/core-component/dashboard/components/cards/active-companies/active-companies.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-active-companies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './active-companies.component.html',
   styleUrl: './active-companies.component.css'
 })
 export class ActiveCompaniesComponent {
   companies = ['IOBTE Company', 'Al madany Company', 'QNP Company'];
 
+constructor(private translate: TranslateService) {
+
+
+}
 }
--- src/app/core-component/dashboard/components/cards/uncompleted-companies/uncompleted-companies.component.ts
+++ src/app/core-component/dashboard/components/cards/uncompleted-companies/uncompleted-companies.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-uncompleted-companies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './uncompleted-companies.component.html',
   styleUrl: './uncompleted-companies.component.css'
 })
@@ -14,4 +15,8 @@ export class UncompletedCompaniesComponent {
     { name: 'Softic Company', progress: ['segment1', 'segment2', ''] },
     { name: 'Al madany company', progress: ['segment1', 'segment2', 'segment3'] }
   ];
+  constructor(private translate: TranslateService) {
+
+
+  }
 }
