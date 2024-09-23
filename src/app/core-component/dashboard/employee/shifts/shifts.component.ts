720d9fa6e28ec26f6142faa9e18813613d5313f3
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-09-23 15:50:53 +0300

    make shifts ui and localization

--- src/app/core-component/dashboard/employee/shifts/shifts.component.ts
+++ src/app/core-component/dashboard/employee/shifts/shifts.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-shifts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule , TranslateModule],
   templateUrl: './shifts.component.html',
   styleUrl: './shifts.component.css'
 })
