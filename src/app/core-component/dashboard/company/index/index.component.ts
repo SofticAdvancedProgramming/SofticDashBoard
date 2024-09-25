e032a7b9b8c3b875dc74159f80587fec0c2ce3dc
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-09-25 13:12:52 +0300

    make pagination in index

--- src/app/core-component/dashboard/company/index/index.component.ts
+++ src/app/core-component/dashboard/company/index/index.component.ts
@@ -4,32 +4,45 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CompanyService } from '../../../../services/comapnyService/company.service';
 import { Company } from '../../../../../models/company';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-index',
   standalone: true,
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css'],
-  imports: [CompanyCardComponent, CommonModule, RouterLink]
+  imports: [CompanyCardComponent, CommonModule, RouterLink, PaginationModule , CommonModule , FormsModule]
 })
 export class IndexComponent implements OnInit {
   companies: Company[] = [];
+  totalItems: number = 0;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
 
   constructor(private companyService: CompanyService) {}
 
   ngOnInit(): void {
-    this.loadCompanies();
+    this.loadCompanies(this.currentPage);
   }
 
-  loadCompanies(): void {
-    this.companyService.loadCompanies({sortIsAsc:true,pageSize:20}).subscribe(
+  loadCompanies(page: number): void {
+    this.companyService.loadCompanies({
+      sortIsAsc: true,
+      pageIndex: page,
+      pageSize: this.itemsPerPage
+    }).subscribe(
       (response: any) => {
-    console.log(response)
-       this.companies=response.data.list
+        this.companies = response.data.list;
+        this.totalItems = response.data.totalRows;
       },
       error => {
         console.error('Error loading companies', error);
       }
     );
+  }
+  handlePageChange(event: { page: number }): void {
+    this.currentPage = event.page;
+    this.loadCompanies(this.currentPage);
   }
 }
