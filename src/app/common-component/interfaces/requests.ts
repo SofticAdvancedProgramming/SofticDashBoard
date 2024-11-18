d5d6de785b7f9f4dfd00fa2b33d27d510bd478d8
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-11-18 12:07:09 +0200

    display requests

--- src/app/common-component/interfaces/requests.ts
+++ src/app/common-component/interfaces/requests.ts
@@ -1,96 +1,103 @@
 export interface RequestAttachment {
-    id: number;
-    companyId: number;
-    file: string;
-    fileExtension: string;
-    requestId?: number;
-  }
-  
-  export interface Request {
-    id: number;
-    companyId: number;
-    employeeId: number;
-    requestTypeId: number;
-    requestStatusId: number;
-    startDate: string;
-    endDate: string;
-    reason: string;
-    isDelete?: boolean;
-    requestAttachments: RequestAttachment[];
-    employee?: Employee;
-    pageIndex?: number;
-    pageSize?: number;
-  }
-  export enum RequestStatus {
-    ALL=-1,
-    Accepted = 1,
-    Pending = 2,
-    Rejected = 3
-  }
-  export interface RequestAction {
-    id: number;
-    requestStatusId: number;
-  }
-  export interface Employee {
-    firstName: string;
-    lastName: string;
-    fullName: string;
-    jobTitle: string  ;
-    profileImage: string ;
-  }
-  
-  
-  //Reciver Requests
-  
-  
-  export interface ReciverRequestSC {
-    id: number;
-    employeeId: number;
-    requestId: number;
-    receiverId: number;
-    requestStatusId: number;
-    isDelete?: boolean;
-    companyId: number;
-    pageIndex?: number;
-    pageSize?: number;
-  }
-  
-  
-  export interface Receiver {
-    firstName: string;
-    lastName: string;
-    fullName: string;
-    jobTitle: string;
-    profileImage: string | null;
-  }
-  
-  export interface ReciverRequestStatus {
-    name: string;
-    nameAr: string;
-    id: number;
-    companyId: number;
-  }
-  
-  export interface ReceivedRequest {
-    requestId: number;
-    request: Request;
-    receiverId: number;
-    receiver: Receiver;
-    requestStatusId: number;
-    requestStatus: ReciverRequestStatus;
-    id: number;
-    companyId: number;
-  }
-  export interface AddRequest {
-    id: number;
-    companyId: number;
-    employeeId: number;
-    requestTypeId: number;
-    requestStatusId: number;
-    startDate: string; 
-    endDate: string;  
-    amount?: number;
-    assetCategoryId?: number;
-    reason: string;
-    requestAttachments: RequestAttachment[];
-  }
+  id: number;
+  companyId: number;
+  file: string;
+  fileExtension: string;
+  requestId?: number;
+}
+
+export interface Request {
+  id: number;
+  companyId: number;
+  employeeId: number;
+  requestTypeId: number;
+  requestStatusId: number;
+  startDate: string;
+  endDate: string;
+  reason: string;
+  isDelete?: boolean;
+  requestAttachments: RequestAttachment[];
+  employee?: Employee;
+  pageIndex?: number;
+  pageSize?: number;
+}
+export enum RequestStatus {
+  ALL=-1,
+  Accepted = 1,
+  Pending = 2,
+  Rejected = 3
+}
+export interface RequestAction {
+  id: number;
+  requestStatusId: number;
+}
+export interface Employee {
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  jobTitle: string  ;
+  profileImage: string ;
+}
+
+
+//Reciver Requests
+
+
+export interface ReciverRequestSC {
+  id: number;
+  requestId: number;
+  receiverId: number;
+  requestStatusId: number;
+  isDelete?: boolean;
+  companyId: number;
+  pageIndex?: number;
+  pageSize?: number;
+  employeeId?:number
+}
+
+
+export interface Receiver {
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  jobTitle: string;
+  profileImage: string | null;
+}
+
+export interface ReciverRequestStatus {
+  name: string;
+  nameAr: string;
+  id: number;
+  companyId: number;
+}
+
+export interface ReceivedRequest {
+  requestId: number;
+  request: Request;
+  receiverId: number;
+  receiver: Receiver;
+  requestStatusId: number;
+  requestStatus: ReciverRequestStatus;
+  id: number;
+  requestTypeId:number;
+  companyId: number;
+  employee?: {
+    firstName?: string;
+    lastName?: string;
+    profileImage?: string;
+    jobTitle?: string;
+  };
+}
+export interface AddRequest {
+  id: number;
+  companyId: number;
+  employeeId: number;
+  requestTypeId: number;
+  requestStatusId: number;
+  startDate: string; 
+  endDate: string;  
+  amount?: number;
+  assetCategoryId?: number;
+  reason: string;
+  requestAttachments: RequestAttachment[];
+}
