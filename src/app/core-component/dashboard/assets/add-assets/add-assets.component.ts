df00d8e012c34c98f162f87cab32c20366d0f9b9
MayarMahmoudd <mayar@sap-pro.co>
2024-12-03 20:05:53 +0200

    integrate add asset

--- src/app/core-component/dashboard/assets/add-assets/add-assets.component.ts
+++ src/app/core-component/dashboard/assets/add-assets/add-assets.component.ts
@@ -1,25 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { TranslateService,TranslateModule } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { LocalStorageService } from '../../../../services/local-storage-service/local-storage.service';
+import { AssetsService } from '../../../../services/AssetsService/assets.service';
 
 @Component({
   selector: 'app-add-assets',
   standalone: true,
-  imports: [TranslateModule,CommonModule],
+  imports: [TranslateModule, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './add-assets.component.html',
-  styleUrl: './add-assets.component.css'
+  styleUrl: './add-assets.component.css',
 })
-export class AddAssetsComponent {
-  constructor(
-     private translate: TranslateService,
-     private route: ActivatedRoute
-  ) { }
+export class AddAssetsComponent implements OnInit {
+  form!: FormGroup;
   selectedFileName: string | null = null;
-
   imagePreviewUrl: string | ArrayBuffer | null = null;
   fileType: string | null = null;
   uploadMessage: string | null = null;
+  companyId = this.localStorageService.getItem('companyId');
+  constructor(
+    private translate: TranslateService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private localStorageService:LocalStorageService,
+    private assetsService: AssetsService
+  ) {}
+  ngOnInit(): void {
+    this.initiation();
+  }
+  initiation(){
+    this.form = this.fb.group({
+      AssetName: ['', Validators.required],
+      AssetID: ['', Validators.required],
+      AssetCategory: ['', Validators.required],
+      AssetSupCategory: ['', Validators.required],
+      AssetReason: ['', Validators.required],
+      AssetAccessories: ['', Validators.required],
+      AssetLocation: ['', Validators.required],
+      AssetAttachment: ['', Validators.required],
+      AssetPhoto: ['', Validators.required]
+    })
+  }
 
   onFileChange(event: any): void {
     const file = event.target.files[0];
@@ -38,7 +61,9 @@ export class AddAssetsComponent {
         const base64String = result.split(',')[1];
         this.addAttachment(base64String, file.name);
         this.uploadMessage = this.translate.instant(
-          this.fileType?.startsWith('image/') ? 'SENT_REQUESTS.IMAGE_UPLOADED' : 'SENT_REQUESTS.FILE_UPLOADED'
+          this.fileType?.startsWith('image/')
+            ? 'SENT_REQUESTS.IMAGE_UPLOADED'
+            : 'SENT_REQUESTS.FILE_UPLOADED'
         );
         if (this.fileType?.startsWith('image/')) {
           this.imagePreviewUrl = result;
@@ -56,5 +81,20 @@ export class AddAssetsComponent {
     //   file: base64String,
     //   fileExtension: extension || '',
     // });
+  }
+
+  onSubmit(){
+    const params ={
+
+    }
+
+    if(this.form.valid){
+      this.assetsService.addAsset(params).subscribe({
+        next: (res) => {
+
+        },
+        error: (err) => console.log(err)
+      })
+    }
   }
 }
