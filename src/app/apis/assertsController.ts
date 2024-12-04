cb23a634d7a5a5d94b341b2f8401a07c8e9ad2cb
MayarMahmoudd <mayar@sap-pro.co>
2024-12-04 16:28:31 +0200

    add asset integration done

--- src/app/apis/assertsController.ts
+++ src/app/apis/assertsController.ts
@@ -1,9 +1,13 @@
 import { environment } from "../environment/environment";
 
 
-export const assetsController = {
+export const assetsCategoryController = {
     getMainAssets: `${environment.apiBaseUrl}AssetCategory/Get`,
     addAsset: `${environment.apiBaseUrl}AssetCategory/Add`,
     editAsset: `${environment.apiBaseUrl}AssetCategory/Edit`,
     deleteAsset: `${environment.apiBaseUrl}AssetCategory/Delete`
 };
+
+export const assetsController = {
+    addAsset: `${environment.apiBaseUrl}Asset/Add`
+}
