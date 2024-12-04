1f2361405cf3873cb752d6f7610543777a19d013
mayar <mayar@sap-pro.co>
2024-12-04 19:55:48 +0200

    asmaa-view assets

--- src/app/core-component/dashboard/assets/show-assets/show-assets.component.ts
+++ src/app/core-component/dashboard/assets/show-assets/show-assets.component.ts
@@ -6,19 +6,22 @@ import { RouterLink } from '@angular/router';
 import { AssetsService } from '../../../../services/AssetsService/assets.service';
 import { Assets } from '../../../../../models/assetsModel';
 import { LocalStorageService } from '../../../../services/local-storage-service/local-storage.service';
+import { FormsModule } from '@angular/forms';
+import { search } from '@tensorflow/tfjs-core/dist/io/composite_array_buffer';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-show-assets',
   standalone: true,
-  imports: [TranslateModule , FilterPopupComponent,CommonModule,RouterLink],
+  imports: [TranslateModule , FilterPopupComponent,CommonModule,RouterLink,FormsModule,PaginationModule],
   templateUrl: './show-assets.component.html',
   styleUrl: './show-assets.component.css'
 })
 export class ShowAssetsComponent {
 
   isFilterPopupVisible = false;
-
   assets: any[] = [];
+  filteredAssets: any[] = [];
   assetsCategory:{
         name: string,
         nameAr: string,
@@ -26,41 +29,48 @@ export class ShowAssetsComponent {
         id: number,
         companyId: number
       }[]=[];
-
   itemsPerPage: number = 10;
-
+  totalPages:number=1;
+  page: number=1;
+  searchText: string = '';
+  companyId:number=0;
+  currentPage: number = 1;
+  totalRows: number = 0;
   constructor(
     private translate: TranslateService,
     private assetsService: AssetsService,
     private localStorageService: LocalStorageService,
   ) {
-    this.getAssetsCategory(1);
+    this.companyId=Number(localStorage.getItem('companyId'));
+    this.getAssetsCategory();
     this.gettAssets();
   }
-  get isArabic(): boolean {
-    return localStorage.getItem('lang') === 'ar';
-  }
 
-  getAssetsCategory(page: number){
+  getAssetsCategory(page?:number){
     const companyId = Number(this.localStorageService.getItem('companyId'));
     const params = {
       companyId,
-      pageIndex: page,
+      pageIndex: this.page,
       pageSize: this.itemsPerPage,
     };
     this.assetsService.getMainAssetsCategory(params).subscribe(
       res=>{
         this.assetsCategory=res.data.list;
-        console.log(res.data.list);
-        console.log( this.assetsCategory);
+        this.totalPages=res.data.totalPages;
+        // console.log(res.data.list);
+        // console.log( this.assetsCategory);
       }
     )
   }
-  gettAssets(){
-    this.assetsService.getAsset().subscribe({
+  gettAssets(event?:any){
+    const query={"assetCategoryId":event}
+   // console.log(event)
+    this.assetsService.getAsset(query).subscribe({
       next:(res=>{
-        console.log(res.data.list);
+       // console.log(res.data.list);
         this.assets=res.data.list;
+        this.filteredAssets= this.assets
+        this.totalRows = res.data.totalRows;
       }
     ),
       error:(err=>{ console.log(err)})
@@ -76,4 +86,63 @@ export class ShowAssetsComponent {
   onFilterPopupClose(isVisible: boolean) {
     this.isFilterPopupVisible = isVisible;
   }
+  applyFilter() {
+    let query: any = { companyId: this.companyId, pageIndex:this.page };
+    query
+    this.assetsService.getAsset({
+      companyId: this.companyId,
+      pageIndex:this.page,
+      name:this.searchText.trim()
+    }).subscribe({
+      next:(res=>{
+        this.assets=res.data.list;
+        this.filteredAssets= this.assets
+      }
+    ),
+      error:(err=>{ console.log(err)})
+    })
+
+
+    // if (this.searchText.trim()) {
+    // //  console.log(this.searchText)
+    //   this.filteredAssets = this.assets.filter(
+    //     (asset) =>
+    //         asset.assetCategoryName?.toLowerCase()
+    //         .includes(this.searchText.toLowerCase()) ||
+    //         asset.employeeName?.includes(this.searchText) ||
+    //         asset.name?.toLowerCase()
+    //         .includes(this.searchText.toLowerCase()) ||
+    //         asset.nameAr?.toLowerCase()
+    //         .includes(this.searchText.toLowerCase()) ||
+    //       ''
+    //   );
+    // } else {
+    //   this.filteredAssets = [...this.assets];
+    // }
+  }
+
+  get isArabic(): boolean {
+    return localStorage.getItem('lang') === 'ar';
+  }
+  prevPage(){
+    if(this.page>1)
+    this.getAssetsCategory(this.page--);
+  }
+  clear(){
+    this.page=1
+    this.getAssetsCategory(this.page);
+    this.gettAssets();
+  }
+  nextPage(){
+    if(this.page<this.totalPages){
+      this.getAssetsCategory(this.page++);
+    }
+
+  }
+
+  handlePageChange(event: { page: number }) {
+    this.currentPage = event.page;
+    this.gettAssets();
+  }
+
 }
