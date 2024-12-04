f4743f2373a4f201aa52998798bf14cfe55e5f12
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-12-04 16:56:44 +0200

    mayar

---
+++ src/models/assets.ts
@@ -0,0 +1,14 @@
+export interface Asset {
+    id: number;
+    companyId: number;
+    name: string;
+    nameAr: string;
+    model: string;
+    assetCategoryId: number;
+    photo: string;
+    photoExtension: string;
+    long: string;
+    lat: string;
+    employeeId: number;
+    assetCategoryName: string;
+  }
