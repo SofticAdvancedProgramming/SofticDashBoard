212add359c83e6a4b64bb2476f45c233793f941a
Mayarmahmoud1012 <mayar@sap-pro.co>
2024-12-05 13:15:08 +0200

    load employees in drop down

--- src/app/common-component/assign-asset-popup/assign-asset-popup.component.ts
+++ src/app/common-component/assign-asset-popup/assign-asset-popup.component.ts
@@ -2,28 +2,31 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { employee } from '../../../models/employee';
 import { EmployeeService } from '../../services/employeeService/employee.service';
+import { DropDownComponent } from "../../core-component/dashboard/components/drop-down/drop-down.component";
 
 @Component({
-  selector: 'app-assign-asset-popup',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './assign-asset-popup.component.html',
-  styleUrls: ['./assign-asset-popup.component.css']
+    selector: 'app-assign-asset-popup',
+    standalone: true,
+    templateUrl: './assign-asset-popup.component.html',
+    styleUrls: ['./assign-asset-popup.component.css'],
+    imports: [CommonModule, DropDownComponent]
 })
 export class AssignAssetPopupComponent {
   @Output() closeAssignAssets = new EventEmitter<boolean>();
-   @Output() onEmployeeSelected = new EventEmitter<employee>();
+  @Output() onEmployeeSelected = new EventEmitter<employee>();
   @Input() employees: employee[] = [];
   selectedEmployee: employee | null = null;
+  loadingMoreEmployees = false;
+  employeePage = 1;
 
-  constructor(private employeeService: EmployeeService) {}
+  ngOnInit() {
+    this.loadEmployees();
+  }
+  constructor(private employeeService: EmployeeService) { }
 
   closePopup() {
-    this.closeAssignAssets.emit(false);   
+    this.closeAssignAssets.emit(false);
   }
- 
-
- 
   Submit() {
     if (this.selectedEmployee) {
       console.log('Assign an asset to employee applied:', this.selectedEmployee);
@@ -33,12 +36,28 @@ export class AssignAssetPopupComponent {
   }
 
   loadEmployees() {
-    this.employeeService.getEmployees().subscribe({
-      next: (res) => {
-        this.employees = res.data; // assuming res.data contains the employee list
+    if (this.loadingMoreEmployees) return;
+    this.loadingMoreEmployees = true;
+
+    this.employeeService.loadEmployees({
+      accountStatus: 1,
+      pageIndex: this.employeePage,
+      pageSize: 10
+    }).subscribe({
+      next: (response) => {
+         const newItems = response.data.list.filter((item: any) => !this.employees.some(a => a.id == item.id))
+          .map((employee: any) => ({
+            ...employee,
+            name: `${employee.firstName} ${employee.lastName}`  
+          }));
+        
+        this.employees = [...this.employees, ...newItems];
+        this.employeePage++;
+        this.loadingMoreEmployees = false;
       },
       error: (err) => {
         console.error('Error loading employees:', err);
+        this.loadingMoreEmployees = false;
       }
     });
   }
@@ -46,4 +65,5 @@ export class AssignAssetPopupComponent {
   onEmployeeSelect(employee: employee) {
     this.selectedEmployee = employee;
   }
+
 }
