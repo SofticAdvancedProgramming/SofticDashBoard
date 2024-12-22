91304af1e325af9ac5c27886c39cba25ce894b2d
MayarMahmoudd <mayar@sap-pro.co>
2024-12-22 16:01:22 +0200

    bug fixed by mostafa

--- src/app/core-component/dashboard/employee/employeeDetailsComponents/advanced-information/type/employee-type/employee-type.component.ts
+++ src/app/core-component/dashboard/employee/employeeDetailsComponents/advanced-information/type/employee-type/employee-type.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { EmployeeTypeService } from '../../../../../../../services/employeeTypeService/employee-type.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-type',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormsModule, CommonModule],
   templateUrl: './employee-type.component.html',
   styleUrl: './employee-type.component.css'
 })
-export class EmployeeTypeComponent {
+export class EmployeeTypeComponent implements OnInit {
+  id!: number;
+  employeeType!: any;
+  private unsubscribe$ = new Subject<void>();
 
+  constructor(private employeeTypeService: EmployeeTypeService, private route: ActivatedRoute){}
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+          takeUntil(this.unsubscribe$)
+        ).subscribe(params => {
+          this.id = Number(params.get('id'));
+          this.getEmployeeTypes();
+        })
+  }
+  getEmployeeTypes(){
+    this.employeeTypeService.getEmployeeType({userId: this.id}).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.employeeType = res.data.list;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+  
 }
