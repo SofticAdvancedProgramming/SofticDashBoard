be0faf827e7dbb815b4d7a9aab9e8c5052887b39
Mayar_mahmoud <mayar@sap-pro.co>
2024-12-26 01:14:24 +0200

    ...

--- src/app/core-component/dashboard/workflow/request-type/request-type-details/request-type-details.component.ts
+++ src/app/core-component/dashboard/workflow/request-type/request-type-details/request-type-details.component.ts
@@ -23,7 +23,8 @@ import { RequestTypeService } from '../../../../../services/requestTypeService/r
 })
 export class RequestTypeDetailsComponent {
   requestId!: number;
-  requestTypeDetails: any = null;
+  requestTypeDetails: any = null; 
+  requestTypes: any[] = []; 
 
   constructor(
     private route: ActivatedRoute,
@@ -38,14 +39,19 @@ export class RequestTypeDetailsComponent {
   }
 
   loadRequestTypeDetails(id: number): void {
-    // this.requestTypeService.getRequestTypeById(id).subscribe({
-    //   next: (res) => {
-    //     this.requestTypeDetails = res.data;
-    //     console.log('Request Type Details:', this.requestTypeDetails);
-    //   },
-    //   error: (err) => {
-    //     console.error('Error fetching request type details:', err);
-    //   },
-    // });
+    this.requestTypeService.getRequestTypeById(id).subscribe({
+      next: (res) => {
+         this.requestTypeDetails = res.data.list.find((item: any) => item.id === id) || null;
+  
+         this.requestTypes = this.requestTypeDetails?.requestTypeConfigs || [];
+        console.log('Request Type Details:', this.requestTypeDetails);
+        console.log('Request Type Configurations:', this.requestTypes);
+      },
+      error: (err) => {
+        console.error('Error fetching request type details:', err);
+      },
+    });
   }
+  
+  
 }
