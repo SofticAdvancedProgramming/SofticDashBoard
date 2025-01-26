d4dc02a6e95fd4975139e0c658c847854bc23b0b
Mayarmahmoud1012 <165417934+MayarMahmoudd@users.noreply.github.com>
2025-01-26 15:25:40 +0300

    solve bug in attendance table

--- src/app/core-component/dashboard/homeIndex/home.component.ts
+++ src/app/core-component/dashboard/homeIndex/home.component.ts
@@ -116,30 +116,10 @@ export class HomeIndexComponent {
     };
     
 
-  public chartData: Object[] = [
-    { Country: this.translateService.translate("Request an advance"), Literacy_Rate: 19.1 },
-    { Country: this.translateService.translate("Delay requests"), Literacy_Rate: 48.1 },
-    { Country: this.translateService.translate("Vacation requests"), Literacy_Rate: 26.8 },
-  ];
+ 
 
-  public data: Object[] = [
-    { x: 'Chrome', y: 61.3, DataLabelMappingName: Browser.isDevice ? 'Chrome: <br> 61.3%' : 'Chrome: 61.3%' },
-    { x: 'Safari', y: 24.6, DataLabelMappingName: Browser.isDevice ? 'Safari: <br> 24.6%' : 'Safari: 24.6%' },
-    { x: 'Edge', y: 5.0, DataLabelMappingName: 'Edge: 5.0%' },
-    { x: 'Samsung Internet', y: 2.7, DataLabelMappingName: Browser.isDevice ? 'Samsung Internet: <br> 2.7%' : 'Samsung Internet: 2.7%' },
-    { x: 'Firefox', y: 2.6, DataLabelMappingName: Browser.isDevice ? 'Firefox: <br> 2.6%' : 'Firefox: 2.6%' },
-    { x: 'Others', y: 3.6, DataLabelMappingName: Browser.isDevice ? 'Others: <br> 3.6%' : 'Others: 3.6%' }
-  ];
-  public donutChartData = {
-    labels: ['Riyadh', 'Jeddah', 'Dammam', 'Other'],
-    datasets: [
-      {
-        data: [20.8, 10.2, 19, 50],
-        backgroundColor: ['#FF9800', '#4CAF50', '#2196F3', '#9E9E9E'],
-        hoverBackgroundColor: ['#FFB74D', '#81C784', '#64B5F6', '#BDBDBD']
-      }
-    ]
-  };
+ 
+ 
 
   columns: any = ['employeeFirstName', 'attendanceType', 'attendanceDate', 'day', 'hour', 'department'];
   columnsAr: any = ['employeeFirstName', 'attendanceType', 'attendanceDate', 'dayAr', 'hour', 'department'];
@@ -160,10 +140,10 @@ export class HomeIndexComponent {
   getAttendances(searchDate = {}, pageIndex?: number) {
     let query: any = pageIndex ? { pageIndex, sortIsAsc: false, sortCol: "attendanceDate" } : { sortIsAsc: false, sortCol: "attendanceDate" };
     this.attendanceService.getAttendances({ ...searchDate, attendanceTypeId: null }).subscribe((res) => {
-      //  console.log(res);
+       console.log("hhhhhhhhhhhhhhhhhh",res);
       this.attendances = {
         ...res,
-        list: res.list.map((item: any) => ({
+        list: res.data.list.map((item: any) => ({
           ...item,
           attendanceDate: this.functionService.formatDate(item.attendanceDate),
           hour: this.functionService.formatHour(item.attendanceDate),
